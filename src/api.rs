7a2f9261e62daa5d2a28308082aac8455b8392f1
Adam Perkowski <adas1per@protonmail.com>
2024-09-24 23:22:48 +0200

    Authentication + basic API func

---
+++ src/api.rs
@@ -0,0 +1,54 @@
+use reqwest::{
+    header::{HeaderMap, HeaderValue, ACCEPT, AUTHORIZATION, USER_AGENT},
+    StatusCode,
+};
+use secrecy::{ExposeSecret, SecretBox};
+use serde_json::json;
+
+const ACCEPT_VND: &str = "application/vnd.github+json";
+const HUBIT_USER_AGENT: &str = "Hubit CLI";
+const GITHUB_API_NAME: &str = "X-GitHub-Api-Version";
+const GITHUB_API_VERSION: &str = "2022-11-28";
+const HUBIT_AUTHORIZATION: &str = "Bearer";
+const REQ_ERROR: &str = "Request failed";
+
+pub fn init() -> reqwest::Client {
+    let client = reqwest::Client::new();
+    client
+}
+
+pub async fn list_assigned_issues(
+    client: reqwest::Client,
+    token: SecretBox<String>,
+) -> Result<(), StatusCode> {
+    let url = "https://api.github.com/issues";
+    let mut headers = HeaderMap::new();
+
+    headers.insert(ACCEPT, HeaderValue::from_static(ACCEPT_VND));
+    headers.insert(USER_AGENT, HeaderValue::from_static(HUBIT_USER_AGENT));
+    headers.insert(
+        GITHUB_API_NAME,
+        HeaderValue::from_static(GITHUB_API_VERSION),
+    );
+    headers.insert(
+        AUTHORIZATION,
+        HeaderValue::from_str(
+            format!("{} {}", HUBIT_AUTHORIZATION, token.expose_secret()).as_str(),
+        )
+        .unwrap(),
+    );
+
+    let request = client.get(url).headers(headers).send().await;
+    let response = request.expect(REQ_ERROR).status();
+
+    match response {
+        StatusCode::OK => {
+            println!("Request succeded.");
+            Ok(())
+        }
+        _ => {
+            eprintln!("{}: {}", REQ_ERROR, response);
+            Err(response)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
+use secrecy::{ExposeSecret, SecretBox};
+use std::{env, fs, io, io::Write, path::Path};
+
+mod api;
+
+#[tokio::main]
+async fn main() {
     println!("Welcome to Hubit!");
+
+    let token_path = Path::new(&env::var("XDG_CONFIG_HOME").unwrap_or_else(|_| {
+        let home = env::var("HOME").unwrap_or_else(|_| ".".to_string());
+        format!("{}/.config", home)
+    }))
+    .join("hubit/github_token");
+
+    if !token_path.exists() {
+        store_pat(&token_path, request_pat()).expect("Failed to store PAT");
+    }
+
+    let token = get_pat(&token_path);
+
+    let request_client = api::init();
+
+    api::list_assigned_issues(request_client, token).await;
+}
+
+fn request_pat() -> SecretBox<String> {
+    print!("Paste your GitHub PAT (Personal Access Token) below.\nYou can generate a PAT here: https://github.com/settings/tokens/new\nThe needed scopes are: repo. It's recommended to set expiration to 'No expiration'.\n> ");
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    SecretBox::new(input.replace("\n", "").into())
+}
+
+fn store_pat(path: &Path, token: SecretBox<String>) -> Result<(), std::io::Error> {
+    let mut file = fs::File::create(path)?;
+    file.write_all(token.expose_secret().as_bytes())?;
+    Ok(())
+}
+
+fn get_pat(path: &Path) -> SecretBox<String> {
+    SecretBox::new(
+        fs::read_to_string(path)
+            .expect("Failed to read token")
+            .into(),
+    )
 }
